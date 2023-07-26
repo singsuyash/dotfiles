ca4fcc402b8b81d3ef7eda24f61a80e5488b8942
Suyash Kumar Singh <suyash@Suyashs-MacBook-Pro.local>
2023-07-26 13:58:47 +0530

    sdkman, speedtest

--- newmacsetup.sh
+++ newmacsetup.sh
@@ -1,59 +1,94 @@
-current_directory=$(pwd)
-
-mkdir installationtemp
-
-# vim
-brew install vim
-
-# homebrew
-if [ -z "$(which brew)" ]
-then
-	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+install_homebrew() {
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 	(echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/suyash/.zprofile
 	eval "$(/opt/homebrew/bin/brew shellenv)"
-else
-    brew update
-	echo "homebrew already installed"
-	echo
-fi
+}
 
-# terminal beautification
-# oh my zsh
-if [ -z "$(which zsh)" ]
-then
-	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-else
-	echo "zsh is already installed"
-	echo
-fi
+install_zsh() {
+    brew install zsh
+    chsh -s /opt/homebrew/bin/zsh
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+}
 
-# powerlevel10k, a zsh theme
-if [ -z "$(brew ls --versions powerlevel10k)" ]
-then
+install_powerlevel10k() {
 	brew install romkatv/powerlevel10k/powerlevel10k
 	echo "source $(brew --prefix)/opt/powerlevel10k/powerlevel10k.zsh-theme" >>~/.zshrc
-else
-	echo "powerlevel10k is already installed"
-	echo
-fi
+}
 
 # zsh autosuggestions
-if [ -z "$(ls ~/.zsh/ | grep zsh-autosuggestions)" ]
-then
+install_zsh_autosuggestions() {
     git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
     echo 'source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh'>>~/.zshrc
-else
-    echo "zsh autosuggestions is already installed"
-fi
+}
 
 # zsh syntax highlighting
-if [ -z "$(ls ~/.zsh/ | grep zsh-syntax-highlighting)" ]
-then
+install_zsh_syntax_highlighting() {
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
     echo 'source ~/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh'>>~/.zshrc
-else
-    echo "zsh syntax highlighting is already installed"
-fi
+}
+
+install_pyenv() {
+    brew install pyenv
+    echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
+    echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
+    echo 'eval "$(pyenv init -)"' >> ~/.zshrc
+}
+
+install_nvm() {
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.4/install.sh | bash
+    export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+}
+
+install_fig() {
+    brew install fig
+    fig login
+    fig
+}
+
+install_1password() {
+    current_directory=$(pwd)
+    mkdir temp
+    cd temp
+    curl https://downloads.1password.com/mac/1Password.zip --output 1Password.zip
+	unzip 1Password.zip
+	open 1Password\ Installer.app
+    sleep 20
+    cd ..
+    rm -rf temp
+    cd $current_directory
+}
+
+install_sdkman() {
+    curl -s "https://get.sdkman.io" | bash
+}
+
+install_ookla_speed_test() {
+    brew tap teamookla/speedtest
+    brew update
+    brew install speedtest --force
+}
+
+[ -z "$(which brew)" ] && install_homebrew
+[ -z "$(which pyenv)" ] && install_pyenv
+[ -z "$(which python3)" ] && pyenv install 3.10.4
+[ -z "$(which git)" ] && brew install git
+[ -z "$(which vim)" ] && brew install vim
+[ -z "$(which zsh)" ] && install_zsh
+[ -z "$(which zip)" ] && brew install zip
+[ -z "$(which unzip)" ] && brew install unzip
+[ -z "$(which tree)" ] && brew install tree 
+[ -z "$(which htop)" ] && brew install htop 
+[ -z "$(which nvm)" ] && install_nvm
+[ -z "$(which node)" ] && nvm install node
+[ -z "$(which fig)" ] && install_fig
+[ -z "$(ls /Applications | grep 1Password.app)" ] && install_1password
+[ -z "$(which op)" ] && brew install --cask 1password/tap/1password-cli
+[ -z "$(brew ls --versions powerlevel10k)" ] && install_powerlevel10k
+[ -z "$(ls ~/.zsh/ | grep zsh-autosuggestions)" ] && install_zsh_autosuggestions
+[ -z "$(ls ~/.zsh/ | grep zsh-syntax-highlighting)" ] && install_zsh_syntax_highlighting
+[ -z "$(which sdkman)" ] && install_sdkman
+[ -z "$(which speedtest)" ] && install_ookla_speed_test
 
 # copy vimrc
 if [ -n "$(ls -halt ~/ | grep vimrc)" ]
@@ -72,104 +107,6 @@ then
 else
     echo "personal tools are already installed"
 fi
-
-# pyenv
-if [ -z "$(which pyenv)" ]
-then
-    brew install pyenv
-    echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
-    echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
-    echo 'eval "$(pyenv init -)"' >> ~/.zshrc
-else
-    echo "pyenv is already installed"
-    echo
-fi
-
-# python3
-if [ -z "$(which python3)" ]
-then
-    pyenv install 3.10.4
-else
-    echo "python3 is already installed"
-fi
-
-# nvm
-if [ -z "$(which nvm)" ]
-then
-    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.4/install.sh | bash
-    export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-else
-    echo "nvm is already installed"
-fi
-
-if [ -z "$(which node)" ]
-then
-    nvm install node
-else
-    echo "node is already installed"
-fi
-
-# utilities
-if [ -z "$(which zip)" ]
-then
-	brew install zip
-else
-	echo "zip already installed"
-	echo
-fi
-
-if [ -z "$(which unzip)" ]
-then
-	brew install unzip
-else
-	echo "unzip already installed"
-	echo
-fi
-
-if [ -z "$(which tree)" ]
-then
-	brew install tree
-else
-	echo "tree already installed"
-	echo
-fi
-
-if [ -z "$(which fig)" ]
-then
-	brew install fig
-	fig login
-	fig
-else
-	echo "fig already installed"
-	echo
-fi
-
-# 1password
-if [ -z "$(ls /Applications | grep 1Password.app)" ]
-then
-	cd installationtemp
-	curl https://downloads.1password.com/mac/1Password.zip --output 1Password.zip
-	unzip 1Password.zip
-	open 1Password\ Installer.app
-	cd $HOME
-else
-	echo "1Password app already installed"
-	echo
-fi
-
-if [ -z "$(which op)" ]
-then
-	brew install --cask 1password/tap/1password-cli
-else
-	echo "1Password cli already installed"
-	echo
-fi
-
-
-
-rm -r installationtemp
-cd $current_directory
 
 echo "Setup 1Password app, then integrate with 1password cli, visit https://developer.1password.com/docs/cli/get-started"
 echo restart terminal
